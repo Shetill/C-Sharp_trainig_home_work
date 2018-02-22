86d11d0680f3cbe8174bae68123e28034a8e4d70
shetill <shetill@hotmail.com>
2018-02-22 15:21:45 +0300

    Урок-4 (пустой)

---
+++ level_1/hw-4/lvl1_lesson4_hw/lvl1_lesson4_hw/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lvl1_lesson4_hw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
