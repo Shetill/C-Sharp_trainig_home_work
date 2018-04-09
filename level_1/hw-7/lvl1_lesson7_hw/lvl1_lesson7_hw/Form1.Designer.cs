fbecc20c96e152c85867659805238d77ed274853
shetill <shetill@hotmail.com>
2018-04-09 17:40:08 +0300

    Задача № 1 решена полностью

--- level_1/hw-7/lvl1_lesson7_hw/lvl1_lesson7_hw/Form1.Designer.cs
+++ level_1/hw-7/lvl1_lesson7_hw/lvl1_lesson7_hw/Form1.Designer.cs
@@ -32,11 +32,15 @@
             this.btnMultiTwo = new System.Windows.Forms.Button();
             this.btnReset = new System.Windows.Forms.Button();
             this.lblResult = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.игратьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnReturn = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnPlusOne
             // 
-            this.btnPlusOne.Location = new System.Drawing.Point(137, 12);
+            this.btnPlusOne.Location = new System.Drawing.Point(162, 31);
             this.btnPlusOne.Name = "btnPlusOne";
             this.btnPlusOne.Size = new System.Drawing.Size(146, 50);
             this.btnPlusOne.TabIndex = 1;
@@ -46,7 +50,7 @@
             // 
             // btnMultiTwo
             // 
-            this.btnMultiTwo.Location = new System.Drawing.Point(137, 68);
+            this.btnMultiTwo.Location = new System.Drawing.Point(162, 87);
             this.btnMultiTwo.Name = "btnMultiTwo";
             this.btnMultiTwo.Size = new System.Drawing.Size(146, 50);
             this.btnMultiTwo.TabIndex = 1;
@@ -56,7 +60,7 @@
             // 
             // btnReset
             // 
-            this.btnReset.Location = new System.Drawing.Point(137, 124);
+            this.btnReset.Location = new System.Drawing.Point(163, 196);
             this.btnReset.Name = "btnReset";
             this.btnReset.Size = new System.Drawing.Size(146, 50);
             this.btnReset.TabIndex = 1;
@@ -68,23 +72,56 @@
             // 
             this.lblResult.AutoSize = true;
             this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblResult.Location = new System.Drawing.Point(12, 9);
+            this.lblResult.Location = new System.Drawing.Point(10, 65);
             this.lblResult.Name = "lblResult";
             this.lblResult.Size = new System.Drawing.Size(23, 25);
             this.lblResult.TabIndex = 2;
             this.lblResult.Text = "0";
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.игратьToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(321, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // игратьToolStripMenuItem
+            // 
+            this.игратьToolStripMenuItem.Name = "игратьToolStripMenuItem";
+            this.игратьToolStripMenuItem.Size = new System.Drawing.Size(69, 24);
+            this.игратьToolStripMenuItem.Text = "Играть";
+            this.игратьToolStripMenuItem.Click += new System.EventHandler(this.игратьToolStripMenuItem_Click);
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.Location = new System.Drawing.Point(162, 143);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(146, 47);
+            this.btnReturn.TabIndex = 4;
+            this.btnReturn.Text = "Отменить ход";
+            this.btnReturn.UseVisualStyleBackColor = true;
+            this.btnReturn.Click += new System.EventHandler(this.btnReturn_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(290, 181);
+            this.ClientSize = new System.Drawing.Size(321, 258);
+            this.Controls.Add(this.btnReturn);
             this.Controls.Add(this.lblResult);
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.btnMultiTwo);
             this.Controls.Add(this.btnPlusOne);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -95,6 +132,9 @@
         private System.Windows.Forms.Button btnMultiTwo;
         private System.Windows.Forms.Button btnReset;
         private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem игратьToolStripMenuItem;
+        private System.Windows.Forms.Button btnReturn;
     }
 }
 
--- level_1/hw-7/lvl1_lesson7_hw/lvl1_lesson7_hw/Form1.cs
+++ level_1/hw-7/lvl1_lesson7_hw/lvl1_lesson7_hw/Form1.cs
@@ -13,8 +13,8 @@ namespace lvl1_lesson7_hw
     public partial class Form1 : Form
     {
         private int countClickComm = 0;
-
         private int finResult = 123;
+        private int returnResult = 0;
 
         public Form1()
         {
@@ -24,6 +24,7 @@ namespace lvl1_lesson7_hw
 
         private void btnReset_Click(object sender, EventArgs e)
         {
+            returnResult = int.Parse(lblResult.Text);
             countClickComm++;
             lblResult.Text = "0";   
         }
@@ -31,6 +32,7 @@ namespace lvl1_lesson7_hw
         private void btnPlusOne_Click(object sender, EventArgs e)
         {
             countClickComm++;
+            returnResult = int.Parse(lblResult.Text); //Сохраним для возможности вернуться на шаг назад
             lblResult.Text = (int.Parse(lblResult.Text)+1).ToString();
             if (finResult == int.Parse(lblResult.Text))                
             {
@@ -42,13 +44,27 @@ namespace lvl1_lesson7_hw
         private void btnMultiTwo_Click(object sender, EventArgs e)
         {
             countClickComm++;
+            returnResult = int.Parse(lblResult.Text); //Сохраним для возможности вернуться на шаг назад
             lblResult.Text = (int.Parse(lblResult.Text) *2).ToString();
             if (finResult == int.Parse(lblResult.Text))
             {
                 var textmsg = "Поздравляем! Вы выиграли. Вы достигли результата за " + countClickComm.ToString() + " ходов";
                 MessageBox.Show(textmsg,"Finish");
             }
-            //MessageBox.Show("Количество ходов =" + countClickComm.ToString());
+           
+        }
+
+        private void игратьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            countClickComm = 0;
+            var textmsg = "Добро пожаловать в игру Удвоитель.\nЦель игры достигнуть числа " + this.finResult.ToString() + " за наименьшее количество ходов";
+            MessageBox.Show(textmsg,"start");
+        }
+
+        private void btnReturn_Click(object sender, EventArgs e)
+        {
+            countClickComm--;//По идее если делаем шаг назад, то нужно уменьшить и кол-во шагов, хотя хз, может быть и правильнее добавить кол-во ходов
+            lblResult.Text = returnResult.ToString();
         }
     }
 }
